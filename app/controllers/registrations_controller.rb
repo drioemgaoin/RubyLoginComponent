a2678997ddedeeccc45a7f88484fdb02b8ecbf9b
Romain Diegoni <romain_diegoni@hotmail.com>
2017-01-31 12:25:27 +0000

    WIP: sign-up funcitonality

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,18 @@ class RegistrationsController < ApplicationController
   end
 
   def sign_up
-    puts "SIGN UP"
+    self.resource = User.new(sign_up_params)
+    response = HTTParty.post("http://localhost:4000/users", body: { user: resource.as_json })
+
+    if response.success?
+      authenticate(self.resource.email)
+      set_flash_message!(:notice, :signed_up)
+      redirect_to root_path
+    end
   end
+
+  protected
+    def sign_up_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
